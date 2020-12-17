22e5b43f8605fec12fd6faa45f8315791b374cb4
jarodriguezbui <josealftel@gmail.com>
2020-12-17 09:02:41 -0500

    back corriendo bien

---
+++ seeders/20201114225211-seed-user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('users', [{
+            name: 'carlos',
+            email: 'ejemplo@gmail.com',
+            password: '$2y$12$dpRrEQZ2w/PO.1icTmPXLOILiWJnjwbb.5GMn1XLysWiFOR1xDIuW',//123456
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }]);
+    },
+
+    down: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('users', null, {});
+
+    }
+};
